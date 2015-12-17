afeeb999205ffd5435da863958bf27871591d644
Cloudox <cloudox@foxmail.com>
2015-12-17 08:52:14 +0800

    Initial Commit

---
+++ GetDeviceInfoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetDeviceInfoDemo
+//
+//  Created by csdc-iMac on 15/12/17.
+//  Copyright (c) 2015年 csdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetDeviceInfoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetDeviceInfoDemo
+//
+//  Created by csdc-iMac on 15/12/17.
+//  Copyright (c) 2015年 csdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
